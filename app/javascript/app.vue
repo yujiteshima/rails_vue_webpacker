8ae050ecc908088b560c1e73dd33449a0bfe3f19
Yuji Teshima <yuji.teshima@gmail.com>
2019-05-21 22:14:37 +0900

    create dev server

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,22 +1,28 @@
 <template>
   <div id="app">
     <p>{{ message }}</p>
+    <h1>{{ message2}}</h1>
   </div>
 </template>
 
 <script>
 export default {
-  data: function () {
+  data: function() {
     return {
-      message: "Hello Vue!"
-    }
+      message: "Hello Vue!",
+      message2: "DreamsComeTrue!"
+    };
   }
-}
+};
 </script>
 
 <style scoped>
 p {
   font-size: 2em;
+  text-align: center;
+}
+h1 {
+  color: blue;
   text-align: center;
 }
 </style>
